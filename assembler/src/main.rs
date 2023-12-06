59874495a8cac744873e8f0c61ebd0a415e7437b
Michael Geuze <michael.geuze@gmail.com>
2023-12-06 14:41:05 +0100

    Added some testes and files for testing

--- assembler/src/main.rs
+++ assembler/src/main.rs
@@ -1,100 +1,11 @@
+#[allow(unused)]
 #[allow(dead_code)]
 
-use std::cmp::Ordering;
-use std::default;
 use std::env;
-use std::fs;
-use std::error::Error;
 use std::fs::File;
-use std::io::BufRead;
-use std::io::BufReader;
 use std::io::Write;
-use std::process;
-use std::io;
 
-pub struct Config {
-    pub file_name: String
-}
-
-
-#[derive(Debug)]
-enum CommandType {
-    ACommand,
-    CCommand,
-    LCommand,
-    None
-}
-
-pub struct HackParser{
-    current_line_count: usize,
-    current_line: String,
-    current_command: Option<CommandType>,
-    reader: BufReader<File>
-}
-
-impl HackParser{
-    fn build(
-        mut args: impl Iterator<Item = String>
-    ) -> Result<Self, Box<dyn Error>> {
-        args.next(); // Skip Path
-        let file_name = args.next().unwrap_or_default();
-        if file_name.is_empty(){
-            return Err(Box::new(io::Error::new(io::ErrorKind::InvalidInput, "No filename provided.")));
-        }
-        let file = File::open::<String>(file_name);
-        let file = match file {
-            Ok(f) => f,
-            Err(e) => return Err(Box::new(e)),
-        };
-
-        let reader = BufReader::new(file);
-        
-        Ok(HackParser {
-            reader,
-            current_line_count: 0,
-            current_line: String::new(),
-            current_command: None
-        })
-    }
-
-    fn has_more_commands(&self) -> bool{
-        !self.reader.buffer().is_empty()
-    }
-
-
-    fn advance(&mut self) -> io::Result<()> {
-        self.current_line.clear();
-        if self.reader.read_line(&mut self.current_line)? > 0 {
-            self.current_line_count += 1;
-            // decode command types
-            if self.current_line.starts_with('@'){
-                println!("found A command: {}", self.current_line);
-                self.current_command = Some(CommandType::ACommand);
-            }else{
-                println!("found C command: {}", self.current_line);
-                self.current_command = Some(CommandType::ACommand);
-            }
-        }
-
-        Ok(())
-    }
-
-    fn command_type(&self) -> &Option<CommandType>{
-        return &self.current_command;
-    }
-
-    fn symbol(&self) -> &str {
-        match self.current_command {
-            Some(ref command) => match command {
-                CommandType::ACommand => self.current_line.strip_prefix('@').unwrap_or(""),
-                CommandType::CCommand | CommandType::LCommand | CommandType::None => &self.current_line,
-            },
-            None => &self.current_line,
-        }
-    }
-    
-} 
-
+use assembler::HackParser;
 
 
 fn main() {
